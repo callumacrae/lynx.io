97b8adc53ebc1bd5fbc281d5c1bcd962f8aed8ce
Callum Macrae <callum@lynxphp.com>
2012-05-10 12:13:32 +0100

    Fixed a bug where page title wasn't being displayed.

--- lib/article.php
+++ lib/article.php
@@ -19,6 +19,7 @@ if (is_readable('articles/' . $slug . '.md')) {
 	$info['tags'] = explode(', ', $info['tags']);
 
 	$tmpl_vars['article'] = $info;
+	$tmpl_vars['page_title'] = $info['title'];
 	$twig->display('article.twig.html', $tmpl_vars);
 } else {
 	throw404();
